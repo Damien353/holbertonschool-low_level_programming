1934b1488cac9e164961405f11b850df96986a3d
Damien353 <10034@holbertonstudents.com>
2024-10-22 12:32:19 +0000

    1-last_digit.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *Return: 
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+	putchar('\n');
+
+	return (0);
