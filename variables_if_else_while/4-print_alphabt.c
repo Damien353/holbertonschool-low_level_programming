113ed6f003b91e8a07d7a83ec99742e2d7501ee6
Damien353 <10034@holbertonstudents.com>
2024-10-23 08:02:22 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,12 @@ int main(void)
 	char alphabet;
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
+
 	if (alphabet != 'q' && alphabet != 'e')
-		putchar (alphabet);
-}	
-		return (0);
+{
+	putchar (alphabet);
+}
+	putchar ('\n');
+
+	return (0);
 }
