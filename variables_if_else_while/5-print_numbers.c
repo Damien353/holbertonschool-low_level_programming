1cf96cff288ad2d4b2349a6535198cff446234ef
Damien353 <10034@holbertonstudents.com>
2024-10-23 08:20:51 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0=success
+ */
+int main(void)
+{
+	char singleD;
+
+	for (singleD = 0; singleD <= 9; singleD++)
+
+{	putchar (singleD);
+}
+	putchar ('\n');
+	return (0);
+}
