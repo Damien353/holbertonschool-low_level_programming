5e26ccedf137e65477c78d82fd21d06538c2be97
Damien353 <10034@holbertonstudents.com>
2024-10-25 12:03:06 +0000

    4-isalpha.c

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower - entry point
+ *@c: lowercase or upper or letter character
+ *Return: 0 = success
+ */
+int _isalpha(int c)
+{
+
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+{
+		return (1);
+}
+	 else
+{
+       	return (0);
+}
+}
