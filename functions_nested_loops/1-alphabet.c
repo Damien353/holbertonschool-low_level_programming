3e900b8ee6149a67d7b7b69511ce902acb42b255
Damien353 <10034@holbertonstudents.com>
2024-10-25 08:50:56 +0000

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - entry point
+ *Return: 0 is success
+ */
+int main(void)
+{
+	int i;
+	char *alphabet = "abcdefghijklmnopqrstuvwxyz\n";
+
+	for (i = 0; i < 27; i++);
+		_putchar(alphabet[i]);
+	return (0);
+}	
