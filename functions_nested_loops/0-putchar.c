92a74b63423d6257fcf48032c56b54311b29adaa
Damien353 <10034@holbertonstudents.com>
2024-10-25 08:31:33 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - entry point
+ *Return: 0=success
+ */
+int main(void)
+{
+	int i;
+	char *str = "_putchar\n";
+
+	for (i = 0; i < 9; i++)
+		_putchar(str[i]);
+	return (0);
+}
+	
