e3b8ccc3a76403d096b9c93448acaf927231aa92
Damien353 <10034@holbertonstudents.com>
2024-10-25 11:45:54 +0000

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower - entry point
+ *Return: 0 = success
+ */
+int _islower(int c)
+{
+	int i;
+
+	if (i >= 'a' && i <= 'z')
+{
+		return 1;
+}
+	else
+{
+		return 0;
+}
+}
