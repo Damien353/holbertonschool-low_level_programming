b730e46edae4be1770b9eb280bfe8650609777b6
Damien353 <10034@holbertonstudents.com>
2024-10-25 12:59:37 +0000

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_last_digit - entry point
+ *@r: efhezof
+ *Return: 0 is success
+ */
+int print_last_digit(int r)
+{
+	int b = (r % 10);
+
+{
+	_putchar(b);
+}
+	return (b);
+}
