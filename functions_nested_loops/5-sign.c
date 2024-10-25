a12819c85c89c5e37178c8873699a6cf320391b0
Damien353 <10034@holbertonstudents.com>
2024-10-25 12:31:48 +0000

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_sign - entry point
+ *@n : patata
+ *Return : 0 is success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+{
+		_putchar("+\n");
+		return (1);
+}
+	else if (n == 0)
+{
+		_putchar("0\n");
+		return (0);
+}
+	else
+{
+		_putchar("-1\n");
+		return (-1);
+}
+}
