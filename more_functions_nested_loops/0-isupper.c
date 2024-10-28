3b2018178e5332bfb3cd7636d59337a97dab8834
Damien353 <10034@holbertonstudents.com>
2024-10-28 09:09:57 +0000

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_isupper - entry point
+ *@c: uppercase
+ *Return: 0 is success
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (c = 'C')
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+}
