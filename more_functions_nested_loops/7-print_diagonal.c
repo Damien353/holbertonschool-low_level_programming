092b0356cf45ba9c51bc8b21510c65a2b61310f6
Damien353 <10034@holbertonstudents.com>
2024-10-28 13:38:01 +0000

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_diagonal - entry point
+ *@n: fref
+ *Return: 0 success
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+{
+	_putchar('/');
+}
+	_putchar ('\n');
+}
