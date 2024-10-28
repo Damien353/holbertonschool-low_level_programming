e739a8a00fbdf97a9f302b45bb1c85d65ad572da
Damien353 <10034@holbertonstudents.com>
2024-10-28 10:56:47 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,12 @@ void more_numbers(void)
 	int i;
 	int n;
 
-	if (n >= 10)
+	for (n =0; n <=10; n++)
 {
-{
-		_putchar(n / 10 + '0');
-}
-		_putchar(n % 10 + '0');
-}
 	for (i = 0; i <= 14; i++)
 {
+		_putchar("%d", n);
+}
 		_putchar('\n');
 }
 }
