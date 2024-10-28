01f2142f09118e478bbe283366d4404279552472
Damien353 <10034@holbertonstudents.com>
2024-10-28 12:51:22 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,6 @@ void more_numbers(void)
 	for (n = 0; n <= 10; n++)
 {
 	for (i = 0; i <= 14; i++)
-{
 	if (i > 10)
 {		
 		_putchar('1');
@@ -21,5 +20,4 @@ void more_numbers(void)
 }
 }
 		_putchar('\n');
-}
 }
