f55f1993c8a2260b8d062ce54b4dd1189e98f321
Damien353 <10034@holbertonstudents.com>
2024-10-28 13:27:01 +0000

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_line - entry point
+ *@n: zer
+ *Return: 0 success
+ */
+void print_line(int n)
+{
+	int i;
+	
+	if (n <= 0)
+{
+	_putchar('\n');
+	return;
+}
+{
+	for (i = 0; i < n; i++)
+{
+	_putchar('_');
+}
+	_putchar ('\n'):
+}
+}
