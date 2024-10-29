cfd71f953d2cb68ed05b396249397f7cc0896f09
Damien353 <10034@holbertonstudents.com>
2024-10-29 14:53:07 +0000

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_array - entry point
+ *@a:ergrt
+ *@n: number of elements of the array to be printed
+ *Return: 0 success
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+{
+	printf("%d", a[i]);
+
+	if (i < n - 1)
+{
+	printf(", ");
+}
+}
+	printf('\n');
+}
