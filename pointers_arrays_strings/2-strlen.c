b14323fc7c182f9f432e40816d41e2c0d84287e6
Damien353 <10034@holbertonstudents.com>
2024-10-29 10:30:24 +0000

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_strlen - entry point
+ *@s: fezihfa
+ *Return: 0 success
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+{
+	length++;
+	s++;
+}
+	return length;
+}
