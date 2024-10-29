7cb71c35113899a2df8c0dfa63e3cd295a552a18
Damien353 <10034@holbertonstudents.com>
2024-10-29 13:49:21 +0000

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *rev_string - entry point
+ *@s: fedv
+ *Return: 0 success
+ */
+void rev_string(char *s)
+{
+	int left = 0;
+	int right = 0;
+
+	while (s[right] != '\0')
+{
+	right++;
+}
+	right--;
+	while (left < right)
+{
+	char temp = s[left];
+	s[left] = s[right];
+	s[right] = temp;
+	left++;
+	right--;
+}
+}
