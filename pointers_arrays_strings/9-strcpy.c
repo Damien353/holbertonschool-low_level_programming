e32b3c7f7fb82214c4239e841ebb28f7c4901b1c
Damien353 <10034@holbertonstudents.com>
2024-10-30 08:32:16 +0000

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_strcpy - entry point
+ *@dest: tampon
+ *@src: pointeur
+ *Return: 0 success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *original_dest = dest;
+
+	while (src != '\0')
+{
+	*dest = *src;
+	dest++;
+	src++;
+}
+	*dest = '\0';
+	return (original_dest);
+}
