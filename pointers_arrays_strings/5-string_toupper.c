f529102ef61f7f24cb5eca36cb63da04650b9e82
Damien353 <10034@holbertonstudents.com>
2024-11-02 13:50:32 +0000

    string toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *string_toupper - entry point
+ *
+ *Return: 0 success
+ */
+char *string_toupper(char *)
+{
+	char *original = str;
+
+	while (*str)
+	{
+		if (*str >= 'a' && *str <= 'z')
+	{
+		*str = *str - ('a' - 'A');
+	}
+		str++;
+	}
+	return (original);
+}
