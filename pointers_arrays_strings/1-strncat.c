028beb5fa75c381962af84269369369bf1290986
Damien353 <10034@holbertonstudents.com>
2024-11-02 12:05:18 +0000

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strncat - entry point
+ *@dest: string
+ *@src: string
+ *@n: caractere
+ *Return: 0 success
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *start = dest;
+
+	while (*dest)
+	{
+		dest++;
+	}
+	while (n > 0 && *src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	dest = '\0';
+	return (start);
