dd9fc0db59d4b9ee190bd10c5022e8e2f181169a
Damien353 <10034@holbertonstudents.com>
2024-11-02 13:14:37 +0000

    strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ *_strcomp - entry point
+ *@s1: string
+ *@s2: string
+ *Return: 0 success
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+	{
+		return (*s1 - *s2);
+	}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
