c4b49cf14de87b3a669bc53ab14736518427e20a
Damien353 <10034@holbertonstudents.com>
2024-11-04 14:25:01 +0000

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,28 +7,28 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int count = 0; //compteur pour la longueur du prefixe
+	unsigned int count = 0; /*compteur pour la longueur du prefixe*/
 	int found;
-	char a;
+	char *a;
 
-	while (*s) //boucle sur chaque caractere de s
+	while (*s) /*boucle sur chaque caractere de s*/
 	{
-		found = 0; // reinitialiser trouve pour chaque caractere de s
-	// verifie si le caractere courant de s est dans accept
-		for (char *a = accept; *a; a++)
+		found = 0; /* reinitialiser trouve pour chaque caractere de s*/
+	/* verifie si le caractere courant de s est dans accept*/
+		for (a = accept; *a; a++)
 		{
 			if (*s == *a)
 		{
-			found = 1; //carecte trouve dans accept
-			break; //sort de la boucle de accept
+			found = 1; /*caractere trouve dans accept*/
+			break; /*sort de la boucle de accept*/
 		}
 		}
 		if (!found)
 		{
-			break; //arrete la boucle si pas trouve
+			break; /*arrete la boucle si pas trouve*/
 		}
-		count++; //incremente le compteur pour le prefixe valide
-		s++; //passe au caractere suivant dans s
+		count++; /*incremente le compteur pour le prefixe valide*/
+		s++; /*passe au caractere suivant dans s*/
 	}
 	return (count);
 }
