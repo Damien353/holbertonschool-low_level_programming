1fbc1931b67673e7a618af3d605176533ebe8f1b
Damien353 <10034@holbertonstudents.com>
2024-11-05 13:59:20 +0000

    8-print_diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,11 +10,11 @@ void print_diagsums(int *a, int size)
 {
 	int sum1 = 0, sum2 = 0;
 	int i;
-
+/*parcourt les elements des deux diagonales*/
 	for (i = 0; i < size; i++)
 {
-	sum1 +=	*(a + i * size + i);
-	sum2 += *(a + i * size + (size - i - 1));
+	sum1 +=	*(a + i * size + i); /*parcourt la premiere diagonale (i, i)*/
+	sum2 += *(a + i * size + (size - i - 1)); /* parcourt la deuxieme diagonale (i, size - i - 1)*/
 }
-		printf("%d, %d\n", sum1, sum2);
+		printf("%d, %d\n", sum1, sum2); /*affiche la somme des deux diagonales*/
 }
