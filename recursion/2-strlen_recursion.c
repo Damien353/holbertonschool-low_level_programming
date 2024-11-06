c1acf3e445d8c9d5bbd8034f5d3fc59bba4eda5b
Damien353 <10034@holbertonstudents.com>
2024-11-06 10:19:45 +0000

    2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_strlen_recursion - return length of a string
+ *@s: caractere
+ *Return: 0 success
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0') /*condition si on atteint la fin de chaine*/
+	{
+		return (0); /*longueur d'une chaine vide est 0*/
+	}
+	return (1 + _strlen_recursion(s + 1)); /*compte le caractere actuel + longueur du reste de la chaine
+}
+
