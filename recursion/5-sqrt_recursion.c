cebbda6d0071bf83de6183915e9b3be99278422c
Damien353 <10034@holbertonstudents.com>
2024-11-06 14:32:47 +0000

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,22 @@
 #include "main.h"
 /**
- *_sqtr_recursion - return natural square root of number
+ *_sqrt_recursion - return natural square root of number
  *@n: number
  *Return: 0 success
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	return (-1); /*la racine carree n'est pas definie dans les entiers*/
-	return (_sqtr_recursion(n, 0));
+	return _sqrt_helper(n, 0); /* Lancer la recherche avec un "guess" initial de 0*/
+}
+
+int _sqrt_helper(int n, int guess)
+{
+	/* Si le carré du "guess" est égal à n, on a trouvé la racine ee*/
+	if (guess * guess == n)
+	return (guess);
+	/*Si le carré du "guess" est supérieur à n, il n'y a pas de racine carrée ex*/
+	if (guess * guess > n)
+	return (-1);
+	/*Sinon, on incrémente "guess" et on continue la récursi*/
+	return _sqrt_helper(n, guess + 1);
 }
