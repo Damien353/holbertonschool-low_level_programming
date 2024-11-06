95ee04c5e701150b6b9d7286fd8537f401e2bad0
Damien353 <10034@holbertonstudents.com>
2024-11-06 14:13:52 +0000

    5-sqrt_recursion.c

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_sqtr_recursion - return natural square root of number
+ *@n: number
+ *Return: 0 success
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	return (-1); /*la racine carree n'est pas definie dans les entiers*/
+	return (_sqtr_helper(n, 0));
+}
