476a8da42fa1a3d9a9c299531671510fb15f8fdd
Damien353 <10034@holbertonstudents.com>
2024-11-07 14:03:18 +0000

    sqrt_recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,22 +1,39 @@
 #include "main.h"
 /**
- *_sqrt_recursion - return natural square root of number
- *@n: number
- *Return: 0 success
+ * sqrt_clc - fonction auxiliare calcul racine carr�
+ * @n: valeur pour lequel le calcul est implémente
+ * @r: candidat racine carré, incrément a chaque appel
+ * Return: retourne racine carré ou -
  */
-int _sqrt_recursion(int n)
+int sqrt_clc(int n, int r)
+/* fonction auxiliare pour calculer la racine carré de */
+/* n = nb dont on cherche la racine carre*/
+/* r = une tentative pour racine carré qui augment a chaque appel */
 {
-	return _sqrt_helper(n, 0); /* Lancer la recherche avec un "guess" initial de 0*/
-}
-
-int _sqrt_helper(int n, int guess)
-{
-	/* Si le carré du "guess" est égal à n, on a trouvé la racine ee*/
-	if (guess * guess == n)
-	return (guess);
-	/*Si le carré du "guess" est supérieur à n, il n'y a pas de racine carrée ex*/
-	if (guess * guess > n)
+	if ((r * r) == n) /* condition de sortie */
+/* si r racine carré de n */
+	return (r);
+/* retourne valeur souhaité */
+	if ((r * r) > n) /* condition de sortie */
+/* si r sup�rieur à n */
 	return (-1);
-	/*Sinon, on incrémente "guess" et on continue la récursi*/
-	return _sqrt_helper(n, guess + 1);
+/* si r trop grand, son carré désse n -> n pas de racine carré entière */
+/* return (-1) utilisé pour signaler une erreur ou valeur non défini */
+	return (sqrt_clc(n, r + 1));
+/* si les une des 2 conditions pas rempli -> appel récursif avec r+1*/
+/* continue jusqu'a ce que une des deux condition rempli */
+}
+/**
+ * _sqrt_recursion - calcule racine carré entière d'un nb
+ * @n: nb dont on cherche la racine carré
+ * Return: retourne racine carré ou -1
+ */
+int _sqrt_recursion(int n) /* fonction principal pour calcul racine carré */
+{
+	if (n < 0) /* condition de sortie */
+/* vérification n est négat*/
+	return (-1);
+/* return (-1) utilisé pour signaler une erreur ou valeur non défi*/
+	return (sqrt_clc(n, 0));
+/*appel sqrt_clc, r = 0 signifie départ de l'incrémentaon*/
 }
