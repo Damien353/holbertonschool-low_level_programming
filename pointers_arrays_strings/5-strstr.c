49b4a2472729408fbdd2923852edf8c7a1318568
Damien353 <10034@holbertonstudents.com>
2024-11-07 09:45:47 +0000

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,11 +10,12 @@ char *_strstr(char *haystack, char *needle)
 {
 	int count;
 
-	for (; haystack[0]; haystack++)
+	for (; haystack[0]; haystack++) /*parcourt caractere un par un et continue tant que non nul*/
 	{
-		for (count = 0; haystack[count] == needle[count]; count++)
+		for (count = 0; haystack[count] == needle[count]; count++) /*compare needle et haystack a partir de ce dernier*/
+			/*tant que egaux continue et incremente, se termine si plus egal ou needle est nul*/
 			;
-		if (!(needle[count]))
+		if (!(needle[count])) /*retourne haystack si needle[count] est nul*/
 			return (haystack);
 	}
 	return (0);
