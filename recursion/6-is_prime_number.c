e2ef8151ed31ed6a15fae7cd5cb88b82990306b8
Damien353 <10034@holbertonstudents.com>
2024-11-07 13:54:55 +0000

    prime_number

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,31 +1,34 @@
 #include "main.h"
 /**
- *is_pirme_number - return 1 in the input
- *@n: number
- *Return: 0 success
+ * is_prime_helper - helper function to check primality recursively
+ * @n: number to check
+ * @i: divisor
+ * Return: 1 if it is or 0 if it's not
+ */
+int is_prime_helper(int n, int i)
+{
+	if (i * i > n)
+/* Si i*i dépasse n, c'est un nombre premier */
+	return (1);
+	if (n % i == 0)
+/* Si n est divisible par i, ce n'est pas un nombre premier */
+	return (0);
+	return (is_prime_helper(n, i + 1));
+/* On ve�rifie avec le prochain i*/
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: the number to check
+ * Return: 1 if prime, 0 if not
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1) /*nombre moins que 2 sont pas premiers*/
-	{
-		return (0);
-	}
-	if (n == 2) /*2 est un nombre premier*/
-	{
-		return (1);
-	}
-	if (n % 2 == 0) /*les nombres pairs autres que 2 sont pas premiers*/
-	{
-		return (0);
-	}
-	for (i = 3; i <= n / 2; i += 2) /*teste les diviseurs de 3 jusqu'a n / 2*/
-	{
-		if (n % i == 0)
-		{
-			return (0); /*si diviseur trouve, pas premiers*/
-		}
-	} /*si aucun diviseur trouve, est premiers*/
+	if (n <= 1)
+/* Si n est inférieur ou égal à 1, ce n'est pas un nombre premier */
+	return (0);
+	if (n == 2)
+/* 2 est un nombre premier */
 	return (1);
+	return (is_prime_helper(n, 2));
+/* On commence à vérifier avec i = 2 */
 }
