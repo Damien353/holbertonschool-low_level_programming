3afb6d3b9ebb696799f1f6c61a72629f2d2baccf
Damien353 <10034@holbertonstudents.com>
2024-11-08 13:25:09 +0000

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  *main - entry point
- *@argc:
- *@argv:
+ *@argc: element number of line command
+ *@argv: array size of argc
  *Return: 0=success
  */
 int main(int argc, char *argv[])
 {
-	printf("%s \n",argv[0]);
+	printf("%s \n", argv[0]);
 	return (0);
 }
