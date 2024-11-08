cd65fdba9136756c259cb366febb48b568f4fe04
Damien353 <10034@holbertonstudents.com>
2024-11-08 13:59:30 +0000

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *@argc: element number of line command
+ *@argv: array size of argc
+ *Return: 0=success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc]);
+	return (0);
+}
