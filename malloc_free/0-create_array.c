d7aad31937436094e584a34e86cae4f13ac58c47
Damien353 <10034@holbertonstudents.com>
2024-11-12 12:21:02 +0000

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
  *create_array - creer une chaine de caractere
  *@size: size of memory to print
@@ -9,16 +8,20 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *array = (char *)malloc(size * sizeof(char)); /*allocation memoire pour le tableau de size*/
+	array = (char *)malloc(size * sizeof(char)); /*allocation memoire pour le tableau de size*/
 
 	if (array == NULL) /*si allocation echoue retourne NULL*/
 	{
 		return (NULL);
 	}
-	memset(array, c, size); /*initialiser le tableau avec le caractere c*/
+	for (i = 0; i < size; i++)
+		array[i] = c;
 	return (array); /*retourner le pointeur vers le tableau alloue et initialise*/
 }
