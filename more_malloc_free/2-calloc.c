9731999a27c0d3b1f7b02ed08f53d9f8969a1fca
Damien353 <10034@holbertonstudents.com>
2024-11-13 16:25:19 +0000

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_calloc - allocate mmemory for array
+ *@nmemb: array
+ *@size: size
+ *Return: 0 success
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *ptr = malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	if (ptr == NULL)
+		return (NULL);
+	/*initialise la memoire allouee a zero*/
+	/*on utilise memset pour remplir la memoire a zero*/
+	for (i = 0; i < nmemb * size; i++)
+		((char *)ptr)[i] = 0;
+	return (ptr);
+}
