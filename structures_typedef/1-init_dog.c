f8f3d462f409c71b02a1e642254222844fd8bf35
Damien353 <10034@holbertonstudents.com>
2024-11-15 11:04:27 +0000

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,13 +8,18 @@
  *@owner:dog propri
  *Return: Always 0
  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
-	{
+{
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return (d);
 	}
 }
