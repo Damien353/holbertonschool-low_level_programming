5248c1202e092e02faa08a163a1f40b54161013d
Damien353 <10034@holbertonstudents.com>
2024-11-15 15:07:17 +0000

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
 #include"dog.h"
+/**
+ *_strlen - entry point
+ *@s: fezihfa
+ *Return: 0 success
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+{
+	length++;
+	s++;
+}
+	return (length);
+}
+/**
+ *_strncpy - entry point
+ *@dest: string
+ *@src: string
+ *@n: caractere
+ *Return: 0 success
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
 /**
  *new_dog - creates a new dog
  *@name: dog name
@@ -12,24 +48,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(dog_t));
 	if (d == NULL)
-	{		
+	{
 		return (NULL);
 	}
-	d->name = malloc(strlen(name) + 1);
+	d->name = malloc(_strlen(name) + 1);
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	strcpy(d->name, name);
-	d->owner = malloc(strlen(owner) + 1);
+	_strncpy(d->name, name);
+	d->owner = malloc(_strlen(owner) + 1);
 	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d);
 		return (NULL);
 	}
-	strcpy(d->owner, owner);
+	_strncpy(d->owner, owner);
 	d->age = age;
 	return (d);
 }
