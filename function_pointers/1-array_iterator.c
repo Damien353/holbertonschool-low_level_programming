250b161523688b1f1c590617f423d99638f6dafb
Damien353 <10034@holbertonstudents.com>
2024-11-20 10:57:08 +0000

    test1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -4,14 +4,12 @@
  *@array: an array
  *@size: size of array
  *@action: pointers to function
- *Return: always 0
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
 	for (i = 0; i < size; i++)
-	{
-		action(array[i]); /*appel de la fonction pointee par action*/
-	}
+	
+	action(array[i]); /*appel de la fonction pointee par action*/
 }
