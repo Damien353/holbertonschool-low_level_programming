cd4ab96bbc4820a838cf939011f102841470b989
Damien353 <10034@holbertonstudents.com>
2024-11-22 13:45:25 +0000

    0-sum_them_all.c_essai2

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -8,18 +8,18 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
-	int i;
-
-	if (n == 0)
-		return (0);
+	unsigned int i;
 
 	va_list args; /*initialise va_list pour acceder aux arguments variadic*/
 	va_start(args, n);
+	
+	if (n == 0)
+		return (0);
 
 	/*additionner les arguments*/
 	for (i = 0; i < n; i++)
 	{
-		sum += va_args(args, int); /*on recupere l'argument et on ajoute a sum*/
+		sum += va_arg(args, int); /*on recupere l'argument et on ajoute a sum*/
 	}
 	va_end(args); /*on nettoie la va_list apres utilisation*/
 	return (sum);
