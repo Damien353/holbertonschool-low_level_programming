56d6b262284fe3ff36a2e0f90aed10e30ec63738
Damien353 <10034@holbertonstudents.com>
2024-12-03 15:26:59 +0000

    betty style pour add node end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,7 +2,7 @@
 /**
  *add_node_end - Ajoute un nouveau noeud a la fin d'une liste_t
  *@head: Un pointeur vers le premier noeud de la liste
- *@str: La chai®ne de caratere a ajouter au nouveau oeud
+ *@str: Chaine de caractere a ajouter au nouveau noeud
  *
  *Return: Adresse du nouvel element ou null en cas echec
  */
@@ -10,31 +10,27 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node, *last_node;
 
-	/*Ve©rifier si strest null*/
 	if (str == NULL)
 	return (NULL);
 
-	/*Allouer de la me©moire pour le nouveu noeud*/
+	/*Allouer de la memoire pour nouveau noeud*/
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 	return (NULL);
 
-	/*Dupliquer la chai®ne e assigne a new_node->str*/
+	/*Duppliquer la chaine et assigner a new_node->str*/
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
-	free(new_node); /*Libe©rer lame©moire allue©e pour le nouvau noeud*/
+	free(new_node); /*Liberer la memoire allouee pour nouveau noeud*/
 	return (NULL);
 	}
-
 	/*Calculer la longueur de la chaine*/
 	new_node->len = 0;
 	while (str[new_node->len] != '\0')
 	new_node->len++;
-
 	/*S'assurer que le pointeur next du nouveau noeud est null*/
 	new_node->next = NULL;
-
 	/*Si la liste vide, nouveau noeud devient tete de liste*/
 	if (*head == NULL)
 	{
