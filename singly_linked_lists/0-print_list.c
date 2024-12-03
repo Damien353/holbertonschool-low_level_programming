825d119bc526bb3fca1b5430597255eb7bac7e0c
Damien353 <10034@holbertonstudents.com>
2024-12-03 10:18:49 +0000

    print_list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,3 +15,25 @@ typedef struct list_s
  */
 size_t print_list(const list_t *h)
 {
+	size_t count = 0;
+
+	/*parcours de la liste*/
+	while (h != NULL)
+	{
+		/*verifier si la chaine str est NULL*/
+		if (h->str == NULL)
+		{
+			printf("[0] ((nil)\n");
+		}
+		else
+		{
+		/*affiche la longueur de la chaine et la chaine elle-meme*/
+		/*%u pour unsigned int*/
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		/*incremente count pour compter les noeuds*/
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
