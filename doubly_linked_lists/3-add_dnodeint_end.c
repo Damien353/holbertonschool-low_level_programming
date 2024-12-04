c7451c38b8a5994f0dfe4172dfc8d65d98393d49
Damien353 <10034@holbertonstudents.com>
2024-12-04 15:31:31 +0000

    3-add_dnodeint_end.c

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -26,7 +26,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 	/*trouver dernier noeud de la liste*/
 	last_node = *head;
-	while (last_node != NULL)
+	while (last_node->next != NULL)
 	{
 		last_node = last_node->next;
 	}
