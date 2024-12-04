d64c949758c6989ecc02cdfbb0549642778dfc38
Damien353 <10034@holbertonstudents.com>
2024-12-04 09:56:21 +0000

    premier essai free_list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ *free_list - Libere une liste list_t
+ *@head: Pointeur vers head de la liste list_t
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);/*libere la chaine str*/
+		free(current_node);/*libere le noeud lui-meme*/
+	}
+}
