ae74870c6a5848b784d853feb5cf1e50486af71f
Damien353 <10034@holbertonstudents.com>
2024-12-07 13:55:14 +0000

    0-binary_to_uint.c

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,6 +11,15 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		retun (0);
-	while (b[i] != '\0')
+	while (b[i] != '\0')/*parcourir la chaine*/
 	{
+		/*si caractere autre que 0 ou 1 est trouve, return 0*/
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		/*deplacer ancien resultat vers gauche et ajouter le bit*/
+		result = result * 2 + (b[i] - '0');
+		i++;
+	}
+	return (result);
+}
 
